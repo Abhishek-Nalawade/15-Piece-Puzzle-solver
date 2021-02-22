e804c233ad252e01b6640d5b800ce7e47936a59e
Abhishek Narendra Nalawade <abhi1793@umd.edu>
2021-02-22 00:43:16 -0500

    created a class for node which can store the parent as well as current node information and defined the move functions

--- Project1.py
+++ Project1.py
@@ -19,3 +19,50 @@ class queue():
 
     def size(self):
         return len(self.pending)
+
+class node():
+    def __init__(self, current, parent):
+        self.current = current
+        self.parent = parent
+
+
+def moveleft(node1):
+    if locy == 0:
+        return
+    else:
+        child = node1.copy()
+        child[locx][locy] = node1[locx][locy - 1]
+        child[locx][locy - 1] = node1[locx][locy]
+        #print(currentnode)
+        return child
+
+
+def moveright(node1):
+    if locy != sh[1] -1:
+        return
+    else:
+        child = node1.copy()
+        child[locx][locy] = node1[locx][locy + 1]
+        child[locx][locy + 1] = node1[locx][locy]
+        #print(currentnode)
+        return child
+
+def moveup(node1):
+    if locx != 0:
+        return
+    else:
+        child = node1.copy()
+        child[locx][locy] = node1[locx - 1][locy]
+        child[locx - 1][locy] = node1[locx][locy]
+        #print(currentnode)
+        return child
+
+def movedown(node1):
+    if locx != sh[0]-1:
+        return
+    else:
+        child = node1.copy()
+        child[locx][locy] = node1[locx + 1][locy]
+        child[locx + 1][locy] = node1[locx][locy]
+        #print(currentnode)
+        return child
